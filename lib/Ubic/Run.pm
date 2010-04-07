287bc3d404366d05bf349c92fc5bdfcec5a34383
Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
2010-04-07 16:51:20 +0400

    Ubic::Run supports --force flag

--- lib/Ubic/Run.pm
+++ lib/Ubic/Run.pm
@@ -15,9 +15,16 @@ Ubic::Run - really simple way to write init scripts
 =cut
 
 use Ubic::Cmd;
+use Getopt::Long;
+use Pod::Usage;
 
 sub import {
     my ($name) = $0 =~ m{^/etc/init\.d/(.+)$} or die "Strange $0";
+    my $force;
+    GetOptions(
+        'f|force' => \$force,
+    ) or die "Unknown option specified";
+
     my ($command, @args) = @ARGV;
     my @names;
     if (@args) {
@@ -29,7 +36,7 @@ sub import {
     Ubic::Cmd->run({
         name => \@names,
         ($command ? (command => $command) : ()),
-        command => $command,
+        ($force ? (force => $force) : ()),
     });
 }
 
