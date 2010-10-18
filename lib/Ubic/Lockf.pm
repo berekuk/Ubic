95c8d06bc8725a2eab6ad257480e9cb23ead0cd2
Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
2010-10-18 22:17:48 +0400

    annoying space at the eol removed

--- lib/Ubic/Lockf.pm
+++ lib/Ubic/Lockf.pm
@@ -68,7 +68,7 @@ OFF by default. Tells to achieve a shared lock. If not set, an exclusive lock is
 =item I<blocking>
 
 ON by default. If unset, a non-blocking mode of flock is used. If this flock fails because the lock is already held by some other process,
-C<undef> is returned. If the failure reason is somewhat different, permissions problems or the 
+C<undef> is returned. If the failure reason is somewhat different, permissions problems or the
 absence of a target file directory for example, an exception is raisen.
 
 =item I<timeout>
