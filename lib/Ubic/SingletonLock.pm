ec4926aa4ff68f82594d7eda96dbd48edf849a1d
Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
2010-10-18 22:15:39 +0400

    Ubic::SingletonLock passes any options to lockf()

--- lib/Ubic/SingletonLock.pm
+++ lib/Ubic/SingletonLock.pm
@@ -28,18 +28,22 @@ our %LOCKS;
 
 =item B<< Ubic::SingletonLock->new($filename) >>
 
+=item B<< Ubic::SingletonLock->new($filename, $options) >>
+
 Construct new singleton lock.
 
 Consequent invocations with the same C<$filename> will return the same object if previous object still exists somewhere in process memory.
 
+Any options will be passed directly to L<Ubic::Lockf>.
+
 =cut
 sub new {
-    my ($class, $file) = validate_pos(@_, 1, 1);
+    my ($class, $file, $options) = validate_pos(@_, 1, 1, 0);
 
     if ($LOCKS{$file}) {
         return $LOCKS{$file};
     }
-    my $lock = lockf($file);
+    my $lock = lockf($file, $options);
     my $self = bless { file => $file, lock => $lock } => $class;
 
     $LOCKS{$file} = $self;
@@ -54,6 +58,15 @@ sub DESTROY {
 }
 
 =back
+
+=head1 BUGS AND CAVEATS
+
+This module is a part of ubic implementation and shouldn't be used in non-core code.
+
+It passes options blindly to Ubic::Lockf, so following code will not work correctly:
+
+    $lock = Ubic::SingletonLock->new("file", { shared => 1 });
+    $lock = Ubic::SingletonLock->new("file"); # this call will just return cached shared lock again
 
 =cut
 
