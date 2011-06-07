20b5e0ade5614cf883cb8501880a1284a4bc3abb
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2011-06-07 21:50:40 +0400

    debianize update to 1.29

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,4 @@
 # vim: ft=crontab
 LOGDIR=/var/log/ubic
 
-* * * * *   root   ubic-watchdog ubic.watchdog    >>$LOGDIR/watch_watchdog.log  2>>$LOGDIR/watch_watchdog.err.log
+* * * * *   root   ubic-watchdog ubic.watchdog    >>$LOGDIR/cron-watchdog.log  2>>$LOGDIR/cron-watchdog.err.log
