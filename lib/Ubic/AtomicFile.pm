89a0f5ced475b11a55da74e7b645a654adc7b88b
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2011-11-09 17:20:19 +0400

    AtomicFile syncs content to disk
    Ext4 fs syncs data and inode metadata to disk separately, and this can cause issues.
    The only solution, according to Theodore Ts'o, is to sync data always.
    See https://bugs.launchpad.net/ubuntu/+source/linux/+bug/317781/comments/54 and Ubic::AtomicFile for details.
    And sorry for the performance hit.

--- lib/Ubic/AtomicFile.pm
+++ lib/Ubic/AtomicFile.pm
@@ -11,6 +11,9 @@ use warnings;
 
     Ubic::AtomicFile::store("blah\n" => "/var/lib/blah");
 
+    # store but don't sync content to disk
+    Ubic::AtomicFile::store("blah\n" => "/var/lib/blah", { sync => 0 });
+
 =head1 FUNCTIONS
 
 =over
@@ -18,22 +21,39 @@ use warnings;
 =cut
 
 use IO::Handle;
+use Params::Validate qw(:all);
 
 =item B<store($data, $file)>
 
+=item B<store($data, $file, $options)>
+
 Store C<$data> into C<$file> atomically. Temporary C<$file.new> will be created and then renamed to C<$file>.
 
+If I<sync> flag is set and false in C<$options> hash then data will not be synced on disk before file is renamed (should be faster, but you can lose your data - see comments in code).
+
 =cut
-sub store($$) {
-    my ($data, $file) = @_;
+sub store($$;$) {
+    my ($data, $file, @options) = @_;
+
+    my $options = validate(@options, {
+        sync => { default => 1 },
+    });
 
     my $new_file = "$file.new";
 
-    # here is an interesting link explaining why we need to do it this way:
-    # https://bugs.launchpad.net/ubuntu/+source/linux/+bug/317781/comments/54
     open my $fh, '>', $new_file or die "Can't open '$new_file' for writing: $!";
     print {$fh} $data or die "Can't print to '$new_file': $!";
     $fh->flush or die "Can't flush '$new_file': $!";
+
+    if ($options->{sync}) {
+        # Here is a link which says why we should do sync too if we don't want to lose data:
+        # https://bugs.launchpad.net/ubuntu/+source/linux/+bug/317781/comments/54
+        #
+        # For some types of atomic files this is important, for others (pidfiles, temp files) it can be too big performance hit.
+        # Every part of ubic code decides for itself.
+        $fh->sync or die "Can't sync '$new_file': $!";
+    }
+
     close $fh or die "Can't close '$new_file': $!";
     rename $new_file => $file or die "Can't rename '$new_file' to '$file': $!";
 }
